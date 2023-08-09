a4ae7709a2e1d11407d20425ea046d184d3a33b2
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2023-08-09 08:39:02 -0600

    Initial Commit

---
+++ PortalBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PortalBook
+//
+//  Created by RangerEmerald on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PortalBook/PortalBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalBookApp.swift
+//  PortalBook
+//
+//  Created by RangerEmerald on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PortalBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
