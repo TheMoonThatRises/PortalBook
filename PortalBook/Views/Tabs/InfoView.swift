be3257cdbec02f77c5a0b391ee595fc7985e6fae
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-08-16 20:35:20 -0600

    fix info navigationlinks

--- PortalBook/Views/Tabs/InfoView.swift
+++ PortalBook/Views/Tabs/InfoView.swift
@@ -25,16 +25,16 @@ struct InfoView: View {
                     NavigationLinkRow(title: "My Info", image: Image(systemName: "person.crop.square.fill"))
                 }
                 NavigationLink {
-                    MyInfoView(client: client,
-                               loadingMessage: $loadingMessage,
-                               errorMessage: $errorMessage)
+                    SchoolInfoView(client: client,
+                                   loadingMessage: $loadingMessage,
+                                   errorMessage: $errorMessage)
                 } label: {
                     NavigationLinkRow(title: "School Info", image: Image(systemName: "building.2.fill"))
                 }
                 NavigationLink {
-                    MyInfoView(client: client,
-                               loadingMessage: $loadingMessage,
-                               errorMessage: $errorMessage)
+                    HealthInfoView(client: client,
+                                   loadingMessage: $loadingMessage,
+                                   errorMessage: $errorMessage)
                 } label: {
                     NavigationLinkRow(title: "Health Info", image: Image(systemName: "heart.text.square.fill"))
                 }
